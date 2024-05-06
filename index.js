8112db74e982a5ce1f62814ead8b2aaa5036d064
sgv3efgwsdv <nsjcanyzinha000@hotmail.com>
2024-05-06 15:48:15 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const factorial = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
+function canFinish(numCourses, prerequisites) {
+  const graph = new Array(numCourses).fill(0).map(() => []);
+  const inDegree = new Array(numCourses).fill(0);
+  for (const [course, pre] of prerequisites) {
+    graph[pre].push(course);
+    inDegree[course]++;
   }
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
+  const queue = [];
+  for (let i = 0; i < numCourses; i++) {
+    if (inDegree[i] === 0) queue.push(i);
   }
-  return result;
-};
+  let count = 0;
+  while (queue.length) {
+    const course = queue.shift();
+    count++;
+    for (const nextCourse of graph[course]) {
+      inDegree[nextCourse]--;
+      if (inDegree[nextCourse] === 0) queue.push(nextCourse);
+    }
+  }
+  return count === numCourses;
+}
